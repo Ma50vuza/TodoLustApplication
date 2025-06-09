8d94cb436a073ff2ee45f4c3690a77dbd143dcc8
Ma50vuza <sanelevuza@hexaura.com>
2025-06-09 12:51:54 +0200

    first commit

--- app/app.py
+++ app/app.py
@@ -3,45 +3,34 @@ import requests
 
 app = Flask (__name__)
 
-@app.route('/', methods=["GET"])
+API_URL = "https://todo-backend-liyx.onrender.com/api/todo_entries"
+
+@app.route('/')
 def index():
-    print(url_for('add_task'))
-    response = requests.get("https://todo-backend-liyx.onrender.com/api/todo_entries")
-    data = response.json()
-    return render_template('index.html', todos=data)
 
-@app.route('/<id>')
-def delete_by_id(id):
-    response = requests.delete(f"https://todo-backend-liyx.onrender.com/api/todo_entries/{id}")
-    
-    return redirect(url_for('index'))
+        response = requests.get(API_URL)
+        todos = response.json()
+        #todos = []
+        return render_template('index.html', todos=todos)
 
-@app.route('/add', methods=["POST"])
-def add_task():
-        name = request.form.get("name")
-        response = requests.post("https://todo-backend-liyx.onrender.com/api/todo_entries", json={'name': name})
-        print(response.status_code)
-        return redirect(url_for('index'))
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form.get('name')
+    if name:
+        requests.post(API_URL, json={"name": name})
+    return redirect('/')
 
-@app.route('/edit/<id>', methods=["POST", "GET"])
-def update_task(id):
-    response = requests.get("https://todo-backend-liyx.onrender.com/api/todo_entries")
-    todos = response.json()
-    todo = ""
-    for todo_ in todos:
-         if todo_['id']==  id:
-              todo = todo_['name']
-    if request.method == "POST":
-         
-        name = request.form.get("name")
-        if name:
-            response = requests.put(f"https://todo-backend-liyx.onrender.com/api/todo_entries/{id}", json={"name": name})
-            return redirect(url_for('index'))
-        else:
-            return redirect(url_for('index'))
-    return render_template("edit.html", todo=todo)
+@app.route('/update/<item_id>', methods=['POST'])
+def update(item_id):
+    name = request.form.get('name')
+    if name:
+        requests.put(f"{API_URL}/{item_id}", json={"name": name})
+    return redirect('/')
 
-
+@app.route('/delete/<item_id>', methods=['POST'])
+def delete(item_id):
+    requests.delete(f"{API_URL}/{item_id}")
+    return redirect('/')
 
 if __name__== '__main__':
     app.run(debug=True)
